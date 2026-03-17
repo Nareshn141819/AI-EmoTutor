be75679ec40ab63d7060158a30f2b062ab626356
Naresh N <nareshn141819@gmail.com>
2026-03-17 19:38:14 +0530

    Add emotion detection using transformers pipeline

---
+++ backend/emotion.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+emotion_classifier = pipeline(
+    "text-classification",
+    model="j-hartmann/emotion-english-distilroberta-base"
+)
+
+def detect_emotion(text):
+    result = emotion_classifier(text)
+    return result[0]['label']
