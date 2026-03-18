fa73a4d65abe310385048e95f644a500822a8096
Naresh N <nareshn141819@gmail.com>
2026-03-18 13:02:18 +0530

    Update emotion.py

--- backend/emotion.py
+++ backend/emotion.py
@@ -1,19 +1,18 @@
 # emotion.py
-
 def detect_emotion(text):
 
     text = text.lower()
 
-    if any(word in text for word in ["confused", "don't understand", "doubt"]):
+    if any(word in text for word in ["confused", "don't understand", "what", "why"]):
         return "confusion"
 
-    elif any(word in text for word in ["angry", "frustrated", "hate"]):
-        return "anger"
-
-    elif any(word in text for word in ["sad", "tired", "upset"]):
+    elif any(word in text for word in ["sad", "tired", "hard", "difficult"]):
         return "sadness"
 
-    elif any(word in text for word in ["happy", "good", "great"]):
+    elif any(word in text for word in ["angry", "frustrated", "annoyed"]):
+        return "anger"
+
+    elif any(word in text for word in ["easy", "great", "understood", "good"]):
         return "joy"
 
     return "neutral"
